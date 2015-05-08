a64831530f8f1986118d1e945e59b0d426f16763
Goran Žuri <goran.zuri@gmail.com>
2015-05-08 11:55:39 +0200

    Removed extra config file
    Removed CSV config file for already downloaded torrents, the app now
    saves only last downloaded

--- HorribleSubsDownload/Program.cs
+++ HorribleSubsDownload/Program.cs
@@ -14,7 +14,10 @@ namespace HorribleSubsDownload
     {
         const string LOCAL_DB = "HorribleSubsDownloaderLocalDB.csv";
         const string EXCLUDE_DB = "HorribleSubsExclude.csv";
-        static List<string> localDbData;
+        /// <summary>
+        /// Stores last downloaded torrent from the DB
+        /// </summary>
+        private static string lastDownloadedTorrent;
         static List<string> newTorrents;
         static List<string> excludeTorrents;
 
@@ -23,36 +26,36 @@ namespace HorribleSubsDownload
         /// </summary>
         /// <param name="fileName"></param>
         /// <returns></returns>
-        static bool DownloadAllFromFile(string fileName) 
+        static bool DownloadAllFromFile(string torrentSavePath, string fileName) 
         {
             var isAlreadyFound = false;
             HtmlDocument doc = new HtmlDocument();
-            doc.Load(fileName);
+            var videoQuality = ConfigurationManager.AppSettings.Get("VideoQuality");
+            if (videoQuality != "720p" && videoQuality != "480p" && videoQuality != "1080p")
+                throw new  ConfigurationException("Video quality must be set on 480p or 720p or 1080p");
 
+            doc.Load(fileName);
             var torrents = doc.DocumentNode.Descendants("a").Where(d =>
-                    d.ParentNode.ParentNode.Attributes.Contains("id") && d.ParentNode.ParentNode.Attributes["id"].Value.Contains("720p")
+                    d.ParentNode.ParentNode.Attributes.Contains("id") && d.ParentNode.ParentNode.Attributes["id"].Value.Contains(videoQuality)
                     && d.ParentNode.Attributes.Contains("class") && d.ParentNode.Attributes["class"].Value.Contains("ind-link") && d.InnerHtml == "Torrent")
                     .Select(x => x.Attributes["href"].Value);
-
-            var torrentFolder = System.Configuration.ConfigurationManager.AppSettings.Get("TorrentFileDestPath");
+            
 
             foreach (var torrent in torrents)
             {
+                //Download torrent
                 using (var client = new WebClient())
                 {
                     var torrentFile = client.DownloadData(torrent);
                     // Try to extract the filename from the Content-Disposition header
                     var torrentFileName = "";
                     if (!String.IsNullOrEmpty(client.ResponseHeaders["Content-Disposition"]))
-                    {
-                        torrentFileName = client.ResponseHeaders["Content-Disposition"].Substring(client.ResponseHeaders["Content-Disposition"].IndexOf("filename=") + 10).Replace("\"", "");
-                    }
+                        torrentFileName = client.ResponseHeaders["Content-Disposition"].Substring(client.ResponseHeaders["Content-Disposition"].IndexOf("filename=", StringComparison.Ordinal) + 10).Replace("\"", "");
+                    else 
+                        continue;
 
-                    Console.WriteLine(torrentFolder);
-                    Console.WriteLine(torrentFileName);
-
-                    var torrentPath = Path.Combine(torrentFolder, torrentFileName);
-                    if (!String.IsNullOrEmpty(torrentFileName) && !localDbData.Contains(torrentFileName) && !isAlreadyFound && !excludeTorrents.Any(x=> torrentFileName.Contains(x)))
+                    var torrentPath = Path.Combine(torrentSavePath, torrentFileName);
+                    if (!String.IsNullOrEmpty(torrentFileName) && torrentFileName != lastDownloadedTorrent && !excludeTorrents.Any(x=> torrentFileName.Contains(x)))
                     {
                         File.WriteAllBytes(torrentPath, torrentFile);
                         newTorrents.Add(torrentFileName);
@@ -62,61 +65,66 @@ namespace HorribleSubsDownload
                     {
                         isAlreadyFound = true;
                         break;
-                    }
-                    
+                    }   
                 }
             }
             return isAlreadyFound;
+        }
+
+        private static void UpdateSetting(string value)
+        {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings["LastDownloadedTorrent"].Value = value;
+            configuration.Save();
+
+            ConfigurationManager.RefreshSection("appSettings");
         }
 
 
         static void Main(string[] args)
         {
             var isFirstRun = false;
+            lastDownloadedTorrent = ConfigurationManager.AppSettings.Get("LastDownloadedTorrent");
             
-            
-            var appDataFolder = "";//Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var appDataFolder = String.Empty;
             if (!String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings.Get("ConfigurationFilesPath")))
                 appDataFolder = ConfigurationManager.AppSettings.Get("ConfigurationFilesPath");
-            var localDbFolder = Path.Combine(appDataFolder, LOCAL_DB);
+            else
+                appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
-            excludeTorrents = new List<string>();
-            if (File.Exists(Path.Combine(appDataFolder, EXCLUDE_DB))) 
-                excludeTorrents = File.ReadAllLines(Path.Combine(appDataFolder, EXCLUDE_DB)).ToList();
-
-            if (!File.Exists(localDbFolder))
-            {
-                var file = File.Create(localDbFolder);
-                file.Close();
-                isFirstRun = true;
-            }
-            localDbData = File.ReadAllLines(localDbFolder).ToList();
             newTorrents = new List<string>();
-
-            if (localDbData.Count == 0)
+            excludeTorrents = new List<string>();
+            if (String.IsNullOrWhiteSpace(lastDownloadedTorrent))
                 isFirstRun = true;
 
+            var torrentFileDestPath = ConfigurationManager.AppSettings.Get("TorrentFileDestPath");
+            if (String.IsNullOrWhiteSpace(torrentFileDestPath))
+                throw new ConfigurationException("TorrentFileDestPath must be defined");
+
+            if (!Directory.Exists(torrentFileDestPath))
+                Directory.CreateDirectory(torrentFileDestPath);
             var pageIndex = 0;
             do
             {
-                var fileName = Path.Combine(appDataFolder, Guid.NewGuid() + ".html");
+                var tempHtmlFileName = Path.Combine(appDataFolder, Guid.NewGuid() + ".html");
 
                 using (var client = new WebClient())
                 {
                     Console.WriteLine("Downloading {0} page from HorribleSubs", pageIndex);
-                    client.DownloadFile("http://horriblesubs.info/lib/latest.php?nextid=" + pageIndex, fileName);
+                    client.DownloadFile("http://horriblesubs.info/lib/latest.php?nextid=" + pageIndex, tempHtmlFileName);
                     ++pageIndex;
                 }
-                var foundExisting = DownloadAllFromFile(fileName);
+                var foundExisting = DownloadAllFromFile(torrentFileDestPath, tempHtmlFileName);
 
-                File.Delete(fileName);
+                File.Delete(tempHtmlFileName);
+                //When system reaches already download torrent it stops
                 if (foundExisting)
                     break;
 
-            } while (isFirstRun ? pageIndex < 1 : true);
+            } while (!isFirstRun || pageIndex < 1);
 
-
-            File.AppendAllLines(localDbFolder, newTorrents);
+            if (newTorrents.Count > 0)
+                UpdateSetting(newTorrents.First());
         }
     }
 }
